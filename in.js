94418e766d062f85345b29414bc5b5349ce2750b
irisbayev <irisbayev0427@gmail.com>
2023-03-01 19:02:53 +0500

    ha

---
+++ in.js
@@ -0,0 +1,7 @@
+let docTitile = document.title;
+window.addEventListener("blur", () =>{
+    document.title = "Come back :(";
+})
+window.addEventListener("focus", () =>{
+    document.title = docTitile;
+})
